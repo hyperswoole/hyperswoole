98835ada218b38bd5613245ff73f28229ff8eda1
he qiang <qiang.he@shoufuyou.com>
2018-07-24 16:34:10 +0800

    hq

--- src/Logging/FileLogHandler.php
+++ src/Logging/FileLogHandler.php
@@ -2,10 +2,34 @@
 namespace Hyperframework\Logging;
 
 use Hyperframework\Common\Config;
-use Hyperframework\Common\FileAppender;
-use Hyperframework\Logging\FileLogHandler as Base;
+use Hyperframework\Common\FormattingLogHandler;
 
-class FileLogHandler extends Base {
+class FileLogHandler extends FormattingLogHandler {
+    private $path;
+
+    /**
+     * @return string
+     */
+    protected function getPath() {
+        if ($this->path === null) {
+            $this->path = Config::getString(
+                $this->getName() . '.path', Config::getString(
+                    'hyperframework.logging.file_handler.path',
+                    'log' . DIRECTORY_SEPARATOR . 'app.log'
+                )
+            );
+        }
+        return $this->path;
+    }
+
+    /**
+     * @param string $log
+     * @return void
+     */
+    protected function handleFormattedLog($log) {
+        FileAppender::append($this->getPath(), $log);
+    }
+
     /**
      * @param string $log
      * @return void
