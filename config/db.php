224476e1013c2d72f5eb4230b5c06e59875e2dfe
he qiang <qiang.he@shoufuyou.com>
2018-07-24 20:05:30 +0800

    hq

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'dsn' => 'mysql:host=47.93.76.136;dbname=shoufuyou_v2;charset=utf8',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=heqiang;charset=utf8',
     'username' => 'root',
-    'password' => 'test1111SFY1024#',
+    'password' => 'tyzZ001!',
     'options' => [PDO::ATTR_EMULATE_PREPARES => false]
 ];
