0380528b61f08eb78c6f604aba5c98a09359da1f
hq <qiang.he@shoufuyou.com>
2018-08-02 23:56:11 +0800

    hq

--- src/Web/RequestProxy.php
+++ src/Web/RequestProxy.php
@@ -3,6 +3,6 @@ namespace Hyperswoole\Web;
 
 class RequestProxy {
     public function __call($method, $args) {
-        call_user_func_array([Request::getEngine(), $method], $args);
+        return call_user_func_array([Request::getEngine(), $method], $args);
     }
 }
--- src/Web/ResponseProxy.php
+++ src/Web/ResponseProxy.php
@@ -3,6 +3,6 @@ namespace Hyperswoole\Web;
 
 class ResponseProxy {
     public function __call($method, $args) {
-        call_user_func_array([Response::getEngine(), $method], $args);
+        return call_user_func_array([Response::getEngine(), $method], $args);
     }
 }
